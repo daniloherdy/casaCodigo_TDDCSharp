07a83633ff2911ecdbc978c66745256d01fdefcd
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-04-26 17:39:06 -0300

    Capítulo 4 -  Explicando Baby steps

---
+++ TesteCalculoSalario/Classes/CalculadoraDeSalario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TesteCalculoSalario.Enumerados;
+
+namespace TesteCalculoSalario.Classes
+{
+    public class CalculadoraDeSalario
+    {
+        public double CalculaSalario(Funcionario funcionario)
+        {
+            if (funcionario.Salario > 3000)
+            {
+                return funcionario.Salario * 0.8;
+            }
+            
+            return funcionario.Salario * 0.9;
+        }
+    }
+}
---
+++ TesteCalculoSalario/Classes/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TesteCalculoSalario.Enumerados;
+
+namespace TesteCalculoSalario.Classes
+{
+    public class Funcionario
+    {
+        public String Nome { get; private set; }
+
+        public double Salario { get; private set; }
+
+        public Cargo Cargo { get; private set; }
+
+        public Funcionario(string nome, double salario, Cargo cargo)
+        {
+            this.Nome = nome;
+            this.Salario = salario;
+            this.Cargo = cargo;
+        }
+
+    }
+}
